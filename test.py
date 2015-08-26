be715f5d6165055a1f54d858846072fdd1158118
Codelegant92 <robinchan1992@gmail.com>
2015-08-25 20:44:23 -0700

    a second test

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
 __author__ = 'yangchen'
+def wordSegment(filePath):
+    f = open(filePath)
+    lineList = []
+    attributeList = []
+    for line in f.readlines():
+        line1 = line.strip()
+        print(line1.__class__)
+        if((len(line1) != 0) and (line1[-1] == ';')):
+            lineList.append(line1.split(' '))
+            if(line1[-2] == ']' or line1[-2] =='"'):
+                attributeList.append(lineList[-1][0])
+            else:
+                attributeList.append(lineList[-1][:-1])
+    return(lineList, attributeList)
+
+
+filePath = './atla'
+expressionList, attribute = wordSegment(filePath)
+print(expressionList)
+print(attribute)
