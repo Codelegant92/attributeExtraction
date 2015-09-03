e37e7df8e8f47b40d3922f0a9d43b117a24c456d
Codelegant92 <robinchan1992@gmail.com>
2015-09-02 20:52:34 -0700

    collect the professional words to formulate a corpus

--- attributeExtraction.py
+++ attributeExtraction.py
@@ -14,8 +14,27 @@ def wordSegment(filePath):
                 attributeList.append(lineList[-1][:-1])
     return(lineList, attributeList)
 
+def readLibrary(path):
+    f = open(path)
+    commandList = []
+    for lines in f:
+        commandList.append(lines.strip('\n'))
+    return(commandList)
+
+def commandsMatch(filePath):
+    commandList = readLibrary('AllCommandersOutput.txt')
+    occurredCommand = []
+    f = open(filePath)
+    for lines in f:
+        for command in commandList:
+            if(lines.find(command+' ') != -1):
+                occurredCommand.append(command)
+                break
+    occurredCommand = set(occurredCommand)
+    print(list(occurredCommand))
 
 filePath = './atla'
-expressionList, attribute = wordSegment(filePath)
-print(expressionList)
-print(attribute)
+#expressionList, attribute = wordSegment(filePath)
+#print(expressionList)
+#print(attribute)
+commandsMatch(filePath)
